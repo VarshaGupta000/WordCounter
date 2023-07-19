a9071931f7afe5989441e9f2b63b191fc35bf9d4
VarshaGupta000 <137172286+VarshaGupta000@users.noreply.github.com>
2023-07-19 22:36:25 +0530

    Add files via upload

---
+++ WordCounter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wordcounter;
+
+/**
+ *
+ * @author varsha
+ */
+public class WordCounter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
